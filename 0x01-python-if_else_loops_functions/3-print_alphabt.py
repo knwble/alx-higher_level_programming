2d9a5fe010424f40c448edbc66dbf1e7a816a364
knwble <noble4peter1@gmail.com>
2023-08-10 21:34:24 +0100

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord("a"), ord("z") + 1):
-        if chr(letter) != 'q' and chr(letter) != 'e':
-                print("{:s}".format(chr(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{:s}".format(chr(letter)), end="")

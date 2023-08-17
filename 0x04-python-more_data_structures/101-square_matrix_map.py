c5104a705c7a8af213a2edf327f83b905c7ccb63
knwble <noble4peter1@gmail.com>
2023-08-17 09:40:36 +0100

    a function that computes the square value of all integers of a matrix using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda i: list(map(lambda i: i ** 2, j[:])), matrix)))
+    return (list(map(lambda j: list(map(lambda i: i ** 2, j[:])), matrix)))

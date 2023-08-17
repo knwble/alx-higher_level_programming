534b3d72030d5e1c03f7b1e52b3a0202461035f1
knwble <noble4peter1@gmail.com>
2023-08-17 10:18:31 +0100

    error fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     total = 0
-    roman_num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for roman in reversed(roman_string):
-        num = roman_num[roman]
-        total += num if total < num * 5 else -num
-    return (total)
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total

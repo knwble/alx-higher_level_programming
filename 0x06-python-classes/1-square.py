210100b886deec84c453533bed4c0a109ea84204
knwble <noble4peter1@gmai.com>
2023-08-29 14:03:25 +0000

    a class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
+# 1-square.py
+"""Define a class Square."""
 
 
 class Square:
-    """This class defines a square."""
-
+    """Represent a square."""
     def __init__(self, size):
         """Initialize a new Square.
 
         Args:
-            size (int): The size of the square
+            size (int): The size of the new square.
         """
         self.__size = size

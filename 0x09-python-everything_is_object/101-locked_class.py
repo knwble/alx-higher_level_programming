171af7ef6299387b8c43243f41df8948febd6065
knwble <noble4peter1@gmail.com>
2023-09-05 05:40:46 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
 
 
-class LockedClass():
+class LockedClass:
+    """Allows only instatiation of an attribute called first_name"""
 
-    __slots__ = 'first_name'
+    __slots__ = ["first_name"]

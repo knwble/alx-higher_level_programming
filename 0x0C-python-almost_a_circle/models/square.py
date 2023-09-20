5b28244ffaa0b6e9d5e59660d6d27e5ae125e254
knwble <noble4peter1@gmail.com>
2023-09-20 19:07:45 +0100

    _str_ fixed

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,5 +17,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns an object string representation"""
-        return ("[Square] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width))
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+            self.width)

5b270d403456f58a3f08cce853ae4010f5c60afb
knwble <noble4peter1@gmail.com>
2023-09-20 18:05:59 +0100

    added dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -129,3 +129,11 @@ class Rectangle(Base):
                 self.__x = kwargs["x"]
             if key == "y":
                 self.__y = kwargs["y"]
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        return {"id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y}

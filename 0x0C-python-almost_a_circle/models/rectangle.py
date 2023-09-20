8f6f416906db762db7545bd54424490cbd668280
knwble <noble4peter1@gmail.com>
2023-09-20 17:09:42 +0100

    added area fixed whitespace

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
 
         self.__y = value
-        
+
     def area(self):
         """Defines the area of rectangle"""
         return (self.__height * self.__width)

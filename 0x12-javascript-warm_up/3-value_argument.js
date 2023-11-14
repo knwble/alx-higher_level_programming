301539bf06d8464c61e9b058b3e26ffb054bb140
knwble <noble4peter1@gmail.com>
2023-11-14 22:51:09 +0100

    errors fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // Check if any arguments are passed
 
 if (process.argv[2] === undefined) {
-    console.log("No argument");
-  } else {
-    console.log(process.argv[2]);
-  }
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}

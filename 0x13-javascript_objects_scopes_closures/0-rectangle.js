ac4a99590cc1205f12ee4efd70aa7201033c59ab
knwble <noble4peter1@gmail.com>
2023-11-15 06:03:06 +0100

    rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * Represents a rectangle
+ */
+class Rectangle {
+  // Class definition
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Represents a rectangle class with width and height attributes
+ */
+class Rectangle {
+    constructor (w, h) {
+      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+  
+  module.exports = Rectangle;

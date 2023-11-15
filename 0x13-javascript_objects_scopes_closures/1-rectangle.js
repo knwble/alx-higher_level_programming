31fadd5dc14fe9f117ebac33c56f43e9390b5201
knwble <noble4peter1@gmail.com>
2023-11-15 07:12:23 +0100

    rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,12 +3,12 @@
  * Represents a rectangle class with width and height attributes
  */
 class Rectangle {
-    constructor (w, h) {
-      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;

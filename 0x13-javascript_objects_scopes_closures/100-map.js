f11890c65b6a292c30d4f508bc9527c24a116f75
knwble <noble4peter1@gmail.com>
2023-11-16 10:05:23 +0100

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const { list } = require('./100-data');
 
-const newList = list.map((val, idx) => val * idx);
 console.log(list);
+
+const newList = list.map((value, index) => value * index);
 console.log(newList);

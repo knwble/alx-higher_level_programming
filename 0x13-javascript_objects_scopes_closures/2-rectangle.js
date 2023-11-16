77460e211d0947f933d79b8a2ae26ac45e9a71c0
knwble <noble4peter1@gmail.com>
2023-11-16 09:53:21 +0100

    javascript rectangle errors fixed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-}
 
-module.exports = Rectangle;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+};

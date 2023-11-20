90a197fa5aa3a1590085fe99ad0ba4119e7ae2ee
knwble <noble4peter1@gmail.com>
2023-11-20 04:05:02 +0100

    taskk 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+This script uses parameterized queries to prevent MySQL injections.
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        # Connect to MySQL server running on localhost at port 3306
+        db_connect = db.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+
+        # Create a cursor object to interact with the database
+        db_cursor = db_connect.cursor()
+
+        # Execute a parameterized query to fetch data securely
+        query = (
+            "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]}
+        )
+        db_cursor.execute(query, {'name': argv[4]})
+
+        # Fetch all the rows from the query result
+        rows_selected = db_cursor.fetchall()
+
+        # Display results
+        for row in rows_selected:
+            print(row)
+
+    except db.Error as e:
+        print("Error:", e)
+
+    finally:
+        # Close the cursor and database connection
+        db_cursor.close()
+        db_connect.close()

f4c6b942f3a521f84fc1b4e883be045d0a99c069
knwble <noble4peter1@gmail.com>
2023-11-20 02:58:44 +0100

    display all values for a state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,48 +1,40 @@
 #!/usr/bin/python3
 """
-This script displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
+Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
-import sys
-import MySQLdb
+import MySQLdb as db
+from sys import argv
 
 if __name__ == '__main__':
     try:
-        # Get command-line arguments
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name_searched = sys.argv[4]
-
         # Connect to MySQL server running on localhost at port 3306
-        db = MySQLdb.connect(
+        db_connect = db.connect(
             host="localhost",
             port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
         )
 
         # Create a cursor object to interact with the database
-        cursor = db.cursor()
+        db_cursor = db_connect.cursor()
 
-        # Create the SQL query using user input and execute it
-        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4])
-        cursor.execute(query, (state_name_searched,))
+        # Create the SQL query with the argument and execute it
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+        db_cursor.execute(query, (argv[4],))
 
         # Fetch all the rows from the query result
-        rows = cursor.fetchall()
+        rows_selected = db_cursor.fetchall()
 
         # Display results
-        for row in rows:
+        for row in rows_selected:
             print(row)
 
-    except MySQLdb.Error as e:
+    except db.Error as e:
         print("Error:", e)
 
     finally:
         # Close the cursor and database connection
-        cursor.close()
-        db.close()
+        db_cursor.close()
+        db_connect.close()

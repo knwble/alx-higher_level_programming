2af705d4e135e3db3d7a504b84b2f886c24c0196
knwble <noble4peter1@gmail.com>
2023-12-02 13:53:58 +0100

    pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Python script that Lists recent commits on a given GitHub repository."""
+"""Python script that Lists recent commits on a given GitHub repository.
 
-
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
 import requests
 
-if __name__ == "__main__":
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
 
+if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
     response = requests.get(url)
     commits = response.json()
-
     try:
         for i in range(10):
-            print(f"{commits[i]['sha']}:
-                  {commits[i]['commit']['author']['name']}")
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
